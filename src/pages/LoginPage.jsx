02667df6cf69ab580b700c562b9072ec6395700b
YuklidD <ict21895@fot.sjp.ac.lk>
2024-07-22 19:24:59 +0530

    Login & Registraion

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,52 +1,36 @@
 import React from 'react';
-import { Input, Button, Checkbox, Tabs } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Tabs } from 'antd';
 import ParticlesComponent from '../components/ParticlesComponent';
-import './Login.css'; // Adjust the path as needed
-import RegistrationComponent from './SignUpPage'; // Adjust the path as needed
+import LoginForm from '../components/LoginForm'; // Adjust the path as needed
+import RegistrationComponent from '../components/SignUpForm'; // Adjust the path as needed
+import '../assets/css/Login.css';
 
 const { TabPane } = Tabs;
 
 const LoginComponent = () => {
     return (
-        <div className="login-container">
+        <div className="page-container">
             <ParticlesComponent />
-            <div className="login-content">
-                <div className="login-header">
-                    <img src="src/assets/images/logo.png" alt="Unimo Logo" className="login-logo" />
-                    <p className="login-subtitle">Access real-time updates on faculty facilities from wherever you're at.<br />Run by students for students.</p>
-                </div>
-                <div className="login-form-container">
-                    <Tabs defaultActiveKey="1" centered>
-                        <TabPane tab="Login" key="1">
-                            <div className="tab-content">
-                                <div className="login-form">
-                                    <Input
-                                        size="large"
-                                        placeholder="Username"
-                                        prefix={<UserOutlined />}
-                                        className="login-input"
-                                    />
-                                    <Input.Password
-                                        size="large"
-                                        placeholder="Password"
-                                        prefix={<LockOutlined />}
-                                        className="login-input"
-                                    />
-                                    <div className="login-options">
-                                        <Checkbox>Remember username</Checkbox>
-                                        <a href="#" className="forgot-password-link">Forgot Password</a>
-                                    </div>
-                                    <Button type="primary" size="large" className="login-button">Login</Button>
+            <div className="login-container">
+                <div className="login-content">
+                    <div className="login-header">
+                        <img src="src/assets/images/logo.png" alt="Unimo Logo" className="login-logo" />
+                        <p className="login-subtitle">Access real-time updates on faculty facilities from wherever you're at.<br />Run by students for students.</p>
+                    </div>
+                    <div className="login-form-container">
+                        <Tabs defaultActiveKey="1" centered>
+                            <TabPane tab="Login" key="1">
+                                <div className="tab-content">
+                                    <LoginForm />
                                 </div>
-                            </div>
-                        </TabPane>
-                        <TabPane tab="Sign Up" key="2">
-                            <div className="tab-content">
-                                <RegistrationComponent />
-                            </div>
-                        </TabPane>
-                    </Tabs> 
+                            </TabPane>
+                            <TabPane tab="Sign Up" key="2">
+                                <div className="tab-content">
+                                    <RegistrationComponent />
+                                </div>
+                            </TabPane>
+                        </Tabs> 
+                    </div>
                 </div>
             </div>
         </div>
