69b273c33e83b709a19403f54636e83fa364e1d8
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-10-28 23:14:23 +0530

    Refactor BarcodeScanner component

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -1,166 +1,184 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect, useRef } from 'react';
-import { Card, Spin, Button } from 'antd';
+import { Card, Spin, Button, message, Avatar } from 'antd';
 import Quagga from 'quagga';
 import CheckInSuccess from './CheckInSuccess'; // Import the new success screen component
 import CheckInOutButton from './CheckInOutButton'; // Import the customized button component
 import '../assets/css/BarcodeScanner.css';
+import newApiRequest from '../utils/apiRequests';
 
 const BarcodeScanner = ({ onCancel, actionType }) => {
-  const [scanning, setScanning] = useState(true); // Start scanning by default
-  const [scanComplete, setScanComplete] = useState(false); // Track if scanning is complete
-  const [scanResult, setScanResult] = useState(null); // Store the scanned result
-  const [scanTime, setScanTime] = useState(null); // Store the scan time
-  const [showSuccessScreen, setShowSuccessScreen] = useState(false); // New state to manage success screen
-  const scannerRef = useRef(null);
-  const quaggaInitialized = useRef(false);
+	const [scanning, setScanning] = useState(true); // Start scanning by default
+	const [scanComplete, setScanComplete] = useState(false); // Track if scanning is complete
+	const [scanResult, setScanResult] = useState(null); // Store the scanned result
+	const [scanTime, setScanTime] = useState(null); // Store the scan time
+	const [showSuccessScreen, setShowSuccessScreen] = useState(false); // New state to manage success screen
+	const [checkInUser, setCheckInUser] = useState(null);
+	const scannerRef = useRef(null);
+	const quaggaInitialized = useRef(false);
 
-  useEffect(() => {
-    startScanner();
+	useEffect(() => {
+		startScanner();
 
-    return () => {
-      stopScanner();
-    };
-  });
+		return () => {
+			stopScanner();
+		};
+	});
 
-  const startScanner = () => {
-    if (quaggaInitialized.current) {
-      Quagga.start();
-      return;
-    }
+	useEffect(() => {
+		const teRegex = /^\d{6}$/;
 
-    Quagga.init(
-      {
-        inputStream: {
-          type: 'LiveStream',
-          constraints: {
-            facingMode: 'environment', // Ensure rear camera is used on mobile
-            width: { ideal: 1280 }, // Higher resolution for better accuracy
-            height: { ideal: 720 }, // Higher resolution for better accuracy
-          },
-          target: scannerRef.current,
-        },
-        locator: {
-          patchSize: 'large', // Larger patch size for better accuracy
-          halfSample: false, // Full sampling for better accuracy
-        },
-        numOfWorkers: navigator.hardwareConcurrency || 4,
-        frequency: 10,
-        decoder: {
-          readers: [
-            'ean_reader',
-            'ean_8_reader',
-            'code_128_reader',
-            'code_39_reader',
-            'upc_reader',
-            'upc_e_reader',
-            'codabar_reader',
-            'i2of5_reader',
-          ],
-        },
-        locate: true,
-      },
-      (err) => {
-        if (err) {
-          console.error('Failed to initialize Quagga:', err);
-          setScanning(false);
-          return;
-        }
-        quaggaInitialized.current = true;
-        Quagga.start();
-      }
-    );
+		if (scanResult && teRegex.test(scanResult)) {
+			newApiRequest(`/api/user/`, 'POST', {
+				teNumber: `TE${scanResult}`,
+			}).then(response => {
+				if (response) {
+					setCheckInUser(response);
+				} else {
+					message.error("User could not be found");
+				}
+			});
+		}
+	}, [scanResult]);
 
-    Quagga.onDetected(handleDetected);
-  };
+	const startScanner = () => {
+		if (quaggaInitialized.current) {
+			Quagga.start();
+			return;
+		}
 
-  const stopScanner = () => {
-    if (quaggaInitialized.current) {
-      Quagga.stop();
-    }
-  };
+		Quagga.init(
+			{
+				inputStream: {
+					type: 'LiveStream',
+					constraints: {
+						facingMode: 'environment', // Ensure rear camera is used on mobile
+						width: { ideal: 1280 }, // Higher resolution for better accuracy
+						height: { ideal: 720 }, // Higher resolution for better accuracy
+					},
+					target: scannerRef.current,
+				},
+				locator: {
+					patchSize: 'large', // Larger patch size for better accuracy
+					halfSample: false, // Full sampling for better accuracy
+				},
+				numOfWorkers: navigator.hardwareConcurrency || 4,
+				frequency: 10,
+				decoder: {
+					readers: [
+						'ean_reader',
+						'ean_8_reader',
+						'code_128_reader',
+						'code_39_reader',
+						'upc_reader',
+						'upc_e_reader',
+						'codabar_reader',
+						'i2of5_reader',
+					],
+				},
+				locate: true,
+			},
+			(err) => {
+				if (err) {
+					console.error('Failed to initialize Quagga:', err);
+					setScanning(false);
+					return;
+				}
+				quaggaInitialized.current = true;
+				Quagga.start();
+			}
+		);
 
-  const handleDetected = (result) => {
-    const currentDateTime = new Date();
-    setScanning(false);
-    setScanComplete(true);
-    setScanResult(result.codeResult.code); // Store the result
-    setScanTime(currentDateTime); // Store the current date and time
-    stopScanner(); // Pause the camera immediately
-  };
+		Quagga.onDetected(handleDetected);
+	};
 
-  const handleCheckInOut = () => {
-    if (scanResult) {
-      // Display the success screen when check-in/check-out is completed
-      setShowSuccessScreen(true);
-    }
-  };
+	const stopScanner = () => {
+		if (quaggaInitialized.current) {
+			Quagga.stop();
+		}
+	};
 
-  const handleCancelScan = () => {
-    // Logic to handle the cancellation of scanning
-    setScanning(false);
-    setScanComplete(false);
-    setScanResult(null);
-    setScanTime(null);
-    onCancel(); // Call the onCancel prop to handle navigation back to home
-  };
+	const handleDetected = (result) => {
+		const currentDateTime = new Date();
+		setScanning(false);
+		setScanComplete(true);
+		setScanResult(result.codeResult.code); // Store the result
+		setScanTime(currentDateTime); // Store the current date and time
+		stopScanner(); // Pause the camera immediately
+	};
 
-  const handleBackToHome = () => {
-    // Logic to go back to the home screen or reset the scanner
-    setShowSuccessScreen(false);
-    onCancel(); // Optional: Call the onCancel prop to handle navigation back to home
-  };
+	const handleCheckInOut = () => {
+		if (scanResult) {
+			// Display the success screen when check-in/check-out is completed
+			setShowSuccessScreen(true);
+		}
+	};
 
-  if (showSuccessScreen) {
-    return <CheckInSuccess onBackToHome={handleBackToHome} actionType={actionType} />;
-  }
+	const handleCancelScan = () => {
+		// Logic to handle the cancellation of scanning
+		setScanning(false);
+		setScanComplete(false);
+		setScanResult(null);
+		setScanTime(null);
+		onCancel(); // Call the onCancel prop to handle navigation back to home
+	};
 
-  return (
-    <div className="barcode-scanner-container">
-      <Card className="scanning-card" bordered={false}>
-        <div className="card-content">
-          <div className="camera-container" ref={scannerRef} />
-          <div className="scanning-status">
-            {scanning ? (
-              <>
-                <Spin size="large" />
-                <span>Scanning...</span>
-              </>
-            ) : (
-              <span>Scan complete</span>
-            )}
-          </div>
-        </div>
-        <div className="scan-result">
-          {scanResult && scanTime && (
-            <div className="result-display">
-              <div className="avatar">
-                <img src="/images/avatar.png" alt="Avatar" />
-              </div>
-              <div className="scan-details">
-                <p><strong>Reg. No:</strong> {`TE${scanResult}`}</p>
-                <p><strong>Name:</strong> Monday</p> {/* Example name */}
-                <p><strong>Time:</strong> {scanTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
-              </div>
-            </div>
-          )}
-        </div>
-        <CheckInOutButton
-          handleCheckInOut={handleCheckInOut}
-          scanComplete={scanComplete}
-          actionType={actionType}
-        />
-        <Button 
-          className="cancel-button" 
-          onClick={handleCancelScan} 
-          type="default"
-          style={{ marginTop: '10px' }}
-        >
-          Cancel
-        </Button>
-      </Card>
-    </div>
-  );
+	const handleBackToHome = () => {
+		// Logic to go back to the home screen or reset the scanner
+		setShowSuccessScreen(false);
+		onCancel(); // Optional: Call the onCancel prop to handle navigation back to home
+	};
+
+	if (showSuccessScreen) {
+		return <CheckInSuccess onBackToHome={handleBackToHome} actionType={actionType} />;
+	}
+
+	return (
+		<div className="barcode-scanner-container">
+			<Card className="scanning-card" bordered={false}>
+				<div className="card-content">
+					<div className="camera-container" ref={scannerRef} />
+					<div className="scanning-status">
+						{scanning ? (
+							<>
+								<Spin size="large" />
+								<span>Scanning...</span>
+							</>
+						) : (
+							<span>Scan complete</span>
+						)}
+					</div>
+				</div>
+				<div className="scan-result">
+					{scanResult && scanTime && (
+						<div className="result-display">
+							<div className="avatar">
+								<Avatar src={checkInUser ? checkInUser.profileImage : '...'} className="avatar" />
+							</div>
+							<div className="scan-details">
+								<p><strong>Reg. No:</strong> {`TE${scanResult}`}</p>
+								<p><strong>Name:</strong> {checkInUser? checkInUser.firstName : '...'} </p>
+								<p><strong>Time:</strong> {scanTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+							</div>
+						</div>
+					)}
+				</div>
+				<CheckInOutButton
+					handleCheckInOut={handleCheckInOut}
+					scanComplete={scanComplete}
+					actionType={actionType}
+				/>
+				<Button
+					className="cancel-button"
+					onClick={handleCancelScan}
+					type="default"
+					style={{ marginTop: '10px' }}
+				>
+					Cancel
+				</Button>
+			</Card>
+		</div>
+	);
 };
 
 export default BarcodeScanner;
--- src/pages/CheckingOfficerDashboard.jsx
+++ src/pages/CheckingOfficerDashboard.jsx
@@ -7,181 +7,179 @@ import '../assets/css/CheckingOfficerDashboard.css'; // Ensure you have the corr
 import newApiRequest from '../utils/apiRequests';
 
 const CheckingOfficerDashboard = ({ role }) => {
-  const [scanning, setScanning] = useState(false);
-  const [actionType, setActionType] = useState(null);
-  const [doctorAvailable, setDoctorAvailable] = useState(true);
-  const [isConfirmVisible, setIsConfirmVisible] = useState(false);
-  const [pendingAvailability, setPendingAvailability] = useState(null);
-  const [checkedUser, setCheckedUser] = useState({})
+	const [scanning, setScanning] = useState(false);
+	const [actionType, setActionType] = useState(null);
+	const [doctorAvailable, setDoctorAvailable] = useState(true);
+	const [isConfirmVisible, setIsConfirmVisible] = useState(false);
+	const [pendingAvailability, setPendingAvailability] = useState(null);
+	const [checkedUser, setCheckedUser] = useState({})
 
-  const handleCheckIn = () => {
-    setScanning(true);
-    setActionType('checkin');
-  };
+	const handleCheckIn = () => {
+		setScanning(true);
+		setActionType('checkin');
+	};
 
-  const handleCheckOut = () => {
-    setScanning(true);
-    setActionType('checkout');
-  };
+	const handleCheckOut = () => {
+		setScanning(true);
+		setActionType('checkout');
+	};
 
-  const handleScan = data => {
+	const handleScan = data => {
 
-    const teRegex = /^TE\d{6}$/i;
+		const teRegex = /^\d{6}$/;
 
-    if (data && teRegex.test(data)) {
-      newApiRequest(`/api/user/`, 'POST', {
-        teNumber: data,
-      }).then(response => {
-        if (response) {
-          setCheckedUser({
-            teNumber: data,
-            phoneNumber: response.mobileNumber,
-          });
-        } else {
-          setCheckedUser({
-            teNumber: data,
-          });
-          message.error("User could not be found");
-          // Hence, user not found in the database, his phone number should be obtained: nivindulakshitha
-          // UI/UX development is needed to be done: nivindulakshitha
-        }
-      });
-      setScanning(false);
-    } else {
-      setScanning(false);
-      message.error('Invalid QR code');
-    }
-  };
+		if (data && teRegex.test(data)) {
+			newApiRequest(`/api/user/`, 'POST', {
+				teNumber: `TE${data}`,
+			}).then(response => {
+				if (response) {
+					setCheckedUser({
+						teNumber: data,
+						phoneNumber: response.mobileNumber,
+					});
+				} else {
+					setCheckedUser({
+						teNumber: data,
+					});
+					message.error("User could not be found");
+				}
+			});
+			setScanning(false);
+		} else {
+			setScanning(false);
+			message.error('Invalid QR code');
+		}
+	};
 
-  useEffect(() => {
-    if (!checkedUser || !Object.hasOwn(checkedUser, 'phoneNumber')) return;
-    
-    let url;
-    if (role === 'CheckingOfficer-medicalCenter') {
-      url = `/api/medical-center/enter`;
-    } else if (role === 'CheckingOfficer-library') {
-      url = `/api/library/enter`;
-    } else {
-      console.error('Unknown role, cannot determine URL');
-      return; // Exit the function if the role is not recognized
-    }
+	useEffect(() => {
+		if (!checkedUser || !Object.hasOwn(checkedUser, 'phoneNumber')) return;
 
-    newApiRequest(url, 'POST', checkedUser).then(response => {
-      if (response && response.success) {
-        message.success('Check-in logging successful');
-      } else {
-        message.error('Check-in logging failed');
-      }
-    }).catch(error => {
-      console.error('Error fetching data:', error.message);
-      message.error('Check-in logging failed');
-    })
+		let url;
+		if (role === 'CheckingOfficer-medicalCenter') {
+			url = `/api/medical-center/enter`;
+		} else if (role === 'CheckingOfficer-library') {
+			url = `/api/library/enter`;
+		} else {
+			console.error('Unknown role, cannot determine URL');
+			return; // Exit the function if the role is not recognized
+		}
 
-  }, [checkedUser]);
+		newApiRequest(url, 'POST', checkedUser).then(response => {
+			if (response && response.success) {
+				message.success('Check-in logging successful');
+			} else {
+				message.error('Check-in logging failed');
+			}
+		}).catch(error => {
+			console.error('Error fetching data:', error.message);
+			message.error('Check-in logging failed');
+		})
 
-  const handleCancel = () => {
-    setScanning(false);
-    setActionType(null);
-  };
+	}, [checkedUser]);
 
-  const showConfirmModal = (status) => {
-    setPendingAvailability(status);
-    setIsConfirmVisible(true);
-  };
+	const handleCancel = () => {
+		setScanning(false);
+		setActionType(null);
+	};
 
-  const handleConfirm = () => {
-    setDoctorAvailable(pendingAvailability);
-    setIsConfirmVisible(false);
-  };
+	const showConfirmModal = (status) => {
+		setPendingAvailability(status);
+		setIsConfirmVisible(true);
+	};
 
-  const handleCancelConfirm = () => {
-    setPendingAvailability(null);
-    setIsConfirmVisible(false);
-  };
+	const handleConfirm = () => {
+		setDoctorAvailable(pendingAvailability);
+		setIsConfirmVisible(false);
+	};
 
-  return (
-    <div className="checking-officer-dashboard">
-      {role === 'CheckingOfficer-medicalCenter' && !scanning && (
-        <div className="doctor-availability-container">
-          <span className="doctor-availability-label">Doctor availability:</span>
-          <div className="doctor-availability">
-            <Button
-              className={`ant-btn ${doctorAvailable ? 'ant-btn-available' : 'ant-btn-unavailable'}`}
-              onClick={() => showConfirmModal(true)}
-              icon={<InfoCircleOutlined />}
-              style={{ backgroundColor: doctorAvailable ? '#72c140' : 'white', color: doctorAvailable ? 'white' : 'black' }}
-            >
-              Available
-            </Button>
-            <Button
-              className={`ant-btn ${!doctorAvailable ? 'ant-btn-unavailable' : 'ant-btn-available'}`}
-              onClick={() => showConfirmModal(false)}
-              icon={<InfoCircleOutlined />}
-              style={{ backgroundColor: !doctorAvailable ? '#F5222D' : 'white', color: !doctorAvailable ? 'white' : 'black' }}
-            >
-              Unavailable
-            </Button>
-          </div>
-        </div>
-      )}
+	const handleCancelConfirm = () => {
+		setPendingAvailability(null);
+		setIsConfirmVisible(false);
+	};
 
-      <Modal
-        title={null}
-        visible={isConfirmVisible}
-        onOk={handleConfirm}
-        onCancel={handleCancelConfirm}
-        centered // This ensures the modal opens in the center of the screen
-        footer={
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            <Button key="back" onClick={handleCancelConfirm} style={{ marginLeft: '10px', marginTop: '5px' }}>
-              Cancel
-            </Button>
-            <Button key="submit" type="primary" onClick={/* handleConfirm */ () => { handleScan("TE104818") }} style={{ marginLeft: '10px', marginTop: '5px' }}>
-              Confirm
-            </Button>
-          </div>
-        }
-      >
-        <div style={{ textAlign: 'center' }}>
-          <ExclamationCircleOutlined style={{ fontSize: '48px', color: '#faad14' }} />
-          <h2>Please confirm again</h2>
-          <p>Your action will update the entire system. Be responsible and reconfirm.</p>
-        </div>
-      </Modal>
+	return (
+		<div className="checking-officer-dashboard">
+			{role === 'CheckingOfficer-medicalCenter' && !scanning && (
+				<div className="doctor-availability-container">
+					<span className="doctor-availability-label">Doctor availability:</span>
+					<div className="doctor-availability">
+						<Button
+							className={`ant-btn ${doctorAvailable ? 'ant-btn-available' : 'ant-btn-unavailable'}`}
+							onClick={() => showConfirmModal(true)}
+							icon={<InfoCircleOutlined />}
+							style={{ backgroundColor: doctorAvailable ? '#72c140' : 'white', color: doctorAvailable ? 'white' : 'black' }}
+						>
+							Available
+						</Button>
+						<Button
+							className={`ant-btn ${!doctorAvailable ? 'ant-btn-unavailable' : 'ant-btn-available'}`}
+							onClick={() => showConfirmModal(false)}
+							icon={<InfoCircleOutlined />}
+							style={{ backgroundColor: !doctorAvailable ? '#F5222D' : 'white', color: !doctorAvailable ? 'white' : 'black' }}
+						>
+							Unavailable
+						</Button>
+					</div>
+				</div>
+			)}
+
+			<Modal
+				title={null}
+				visible={isConfirmVisible}
+				onOk={handleConfirm}
+				onCancel={handleCancelConfirm}
+				centered
+				footer={
+					<div style={{ display: 'flex', justifyContent: 'center' }}>
+						<Button key="back" onClick={handleCancelConfirm} style={{ marginLeft: '10px', marginTop: '5px' }}>
+							Cancel
+						</Button>
+						<Button key="submit" type="primary" onClick={handleConfirm} style={{ marginLeft: '10px', marginTop: '5px' }}>
+							Confirm
+						</Button>
+					</div>
+				}
+			>
+				<div style={{ textAlign: 'center' }}>
+					<ExclamationCircleOutlined style={{ fontSize: '48px', color: '#faad14' }} />
+					<h2>Please confirm again</h2>
+					<p>Your action will update the entire system. Be responsible and reconfirm.</p>
+				</div>
+			</Modal>
 
 
-      {!scanning ? (
-        <div className="options">
-          <Button
-            className="check-button"
-            type="primary"
-            icon={<SwapRightOutlined />}
-            onClick={handleCheckIn}
-            style={{ backgroundColor: '#52C41A', borderColor: 'green' }}
-            size="large"
-          >
-            Check in
-          </Button>
-          <Button
-            className="check-button"
-            type="primary"
-            icon={<SwapRightOutlined />}
-            onClick={handleCheckOut}
-            style={{ backgroundColor: '#F5222D', borderColor: '#F5222D' }}
-            size="large"
-          >
-            Check out
-          </Button>
-        </div>
-      ) : (
-        <BarcodeScanner onScan={handleScan} onCancel={handleCancel} actionType={actionType} />
-      )}
-    </div>
-  );
+			{!scanning ? (
+				<div className="options">
+					<Button
+						className="check-button"
+						type="primary"
+						icon={<SwapRightOutlined />}
+						onClick={handleCheckIn}
+						style={{ backgroundColor: '#52C41A', borderColor: 'green' }}
+						size="large"
+					>
+						Check in
+					</Button>
+					<Button
+						className="check-button"
+						type="primary"
+						icon={<SwapRightOutlined />}
+						onClick={handleCheckOut}
+						style={{ backgroundColor: '#F5222D', borderColor: '#F5222D' }}
+						size="large"
+					>
+						Check out
+					</Button>
+				</div>
+			) : (
+				<BarcodeScanner onScan={handleScan} onCancel={handleCancel} actionType={actionType} />
+			)}
+		</div>
+	);
 };
 
 CheckingOfficerDashboard.propTypes = {
-  role: PropTypes.string.isRequired,
+	role: PropTypes.string.isRequired,
 };
 
 export default CheckingOfficerDashboard;
