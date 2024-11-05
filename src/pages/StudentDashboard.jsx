8f99d790013d48ead2efdbcc0f283eb926786d84
Nivindu Lakshitha <nivindulakshitha@gmail.com>
2024-11-05 12:52:31 +0530

    Enhance styling for GreetingSection and StudentDashboard, including avatar border, padding adjustments, and text transformations for hero names

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -386,19 +386,19 @@ const Dashboard = ({ userId, userName }) => {
 											<>
 												<Col xs={24} sm={8}>
 													<Card className="hero-card" cover={<img src="https://unimo.blob.core.windows.net/unimo/Dinning Dynamo.png" alt="Dining Dynamo" />}>
-														<Card.Meta title="Dining Dynamo" description={userTopRankings[1] && `${userTopRankings[1].firstName} ${userTopRankings[1].lastName}`} />
+														<Card.Meta title="Dining Dynamo" className='hero-names' description={userTopRankings[1] && `${userTopRankings[1].firstName} ${userTopRankings[1].lastName}`} />
 														{userTopRankings[1] && (<Text>{userTopRankings[1].entries}  Entries in a row</Text>)}
 													</Card>
 												</Col>
 												<Col xs={24} sm={8} className="hero-card-big">
 													<Card className="hero-card" cover={<img src="https://unimo.blob.core.windows.net/unimo/Canteen Champion.png" alt="Canteen Champion" />}>
-														<Card.Meta title="Canteen Champion" description={userTopRankings[0] && `${userTopRankings[0].firstName} ${userTopRankings[0].lastName}`} />
+														<Card.Meta title="Canteen Champion" className='hero-names' description={userTopRankings[0] && `${userTopRankings[0].firstName} ${userTopRankings[0].lastName}`} />
 														{userTopRankings[0] && (<Text>{userTopRankings[0].entries}  Entries in a row</Text>)}
 													</Card>
 												</Col>
 												<Col xs={24} sm={8}>
 													<Card className="hero-card" cover={<img src="https://unimo.blob.core.windows.net/unimo/Foodie Forcaster.png" alt="Foodie Forecaster" />}>
-														<Card.Meta title="Foodie Forecaster" description={userTopRankings[2] && `${userTopRankings[2].firstName} ${userTopRankings[2].lastName}`} />
+														<Card.Meta title="Foodie Forecaster" className='hero-names' description={userTopRankings[2] && `${userTopRankings[2].firstName} ${userTopRankings[2].lastName}`} />
 														{userTopRankings[2] && (<Text>{userTopRankings[2].entries}  Entries in a row</Text>)}
 													</Card>
 												</Col>
